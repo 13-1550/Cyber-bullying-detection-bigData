589a71daa947daf9449dac5c2a46c2585ff19b7f
Howyna K <93727365+13-1550@users.noreply.github.com>
2026-04-28 14:19:21 +0300

    Implement Kafka producer for data ingestion
    Add Kafka producer to send streaming data.

--- src/ingestion/kafka_producer.py
+++ src/ingestion/kafka_producer.py
@@ -1,1 +1,12 @@
+from kafka import KafkaProducer
+import json
+#collecting streaming data
+producer = KafkaProducer(
+  bootstrap_servers='localhost:9092",
+  value_serializer=lambda v: json.dumps(v).encode('utf-8')
+  )
 
+  data = {"text": "you are stupid", "label":1}
+
+procucer.send('cyberbullying-topic', data)
+producer.flush()
