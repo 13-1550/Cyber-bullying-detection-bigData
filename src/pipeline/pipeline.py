93a8fe1213ec3a1265e7e908650d2bf68b11ebfd
Howyna K <93727365+13-1550@users.noreply.github.com>
2026-05-01 22:58:47 +0300

    Update pipeline.py

--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -1,1 +1,11 @@
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import Tokenizer, NGram, HashingTF, IDF
 
+
+def build_pipeline():
+    tokenizer = Tokenizer(inputCol="lemmatized", outputCol="words")
+    ngram = NGram(n=2, inputCol="words", outputCol="bigrams")
+    hashingTF = HashingTF(inputCol="bigrams", outputCol="rawFeatures")
+    idf = IDF(inputCol="rawFeatures", outputCol="features")
+
+    return Pipeline(stages=[tokenizer, ngram, hashingTF, idf])
