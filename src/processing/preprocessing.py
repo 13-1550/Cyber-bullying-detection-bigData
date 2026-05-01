beb158cd10b3fa48f40762197de0866d8af1c303
Howyna K <93727365+13-1550@users.noreply.github.com>
2026-05-01 22:21:12 +0300

    Implement text lemmatization with spaCy
    Added lemmatization functions using spaCy for text preprocessing.

--- src/processing/preprocessing.py
+++ src/processing/preprocessing.py
@@ -1,1 +1,19 @@
+import spacy
+from pyspark.sql.functions import udf
+from pyspark.sql.types import ArrayType, StringType
 
+nlp = spacy.load("en_core_web_sm")
+
+def lemmatize_spacy(words):
+    doc = nlp(" ".join(words))
+    return [token.lemma_ for token in doc]
+def preprocess(text):
+    doc = nlp(text)
+    return " ".join([
+        token.lemma_
+        for token in doc
+        if not token.is_stop and token.is_alpha
+    ])
+lemma_udf = udf(lemmatize_spacy, ArrayType(StringType()))
+
+train['lemmatized'] = train['text'].apply(preprocess)
